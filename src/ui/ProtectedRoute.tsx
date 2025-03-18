53d7f5cb18f38ce9811ed4c7d2a1c73dd93b8c15
tojamrcel <marcelgala42@gmail.com>
2025-03-18 20:54:02 +0100

    finish implementation of TypeScript

--- src/ui/ProtectedRoute.tsx
+++ src/ui/ProtectedRoute.tsx
@@ -1,8 +1,9 @@
 import { Navigate } from "react-router-dom"
 import { useQuizzes } from "../contexts/QuizzesContext"
 import Spinner from "./Spinner"
+import { ReactNode } from "react"
 
-function ProtectedRoute({ children }) {
+function ProtectedRoute({ children }: { children: ReactNode }) {
     const { status, activeQuiz } = useQuizzes()
     const isLastQuestion =
         activeQuiz.currentQuestion + 1 === activeQuiz.questions.length
